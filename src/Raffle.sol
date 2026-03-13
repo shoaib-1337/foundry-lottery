9d0079d62d0f8dc18f515cabe1e75e80a514da0e
Shoaib <sali18089@gmail.com>
2026-03-13 22:39:27 +0500

    Added RandomWordsRequest function

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -3,92 +3,82 @@ pragma solidity ^0.8.19;
 
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 
+import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
 /**
- * @title Raffle Contract 
+ * @title Raffle Contract
  * @author Shoaib Ali
  * @notice This contract is for creating a sample raffle
  * @dev Implements Chainlink VRFv2.5
  */
 
-
-
 contract Raffle is VRFConsumerBaseV2Plus {
-
     //Errors
     error Raffle__SendMoreToEnterRaffle();
 
     //State Varibales
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
     uint256 private immutable i_entranceFee;
     uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint256 private immutable i_subscriptionId;
+    uint32 private immutable i_CallbackGasLimit;
     address payable[] private s_players;
     uint256 private s_lastTimeStamp;
-
-
 
     //Events
     event RaffleEntered(address indexed player);
 
-
-    constructor(uint256 entranceFee, uint256 interval, address vrfCoordinator) VRFConsumerBaseV2Plus(vrfCoordinator)
-    {
-
-        i_entranceFee=entranceFee;
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint256 subscriptionId,
+        uint32 CallbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
-
+        i_keyHash = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_CallbackGasLimit = CallbackGasLimit;
     }
 
     function enterRaffle() external payable {
-
         //require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
-        if(msg.value<i_entranceFee)
-        {
+        if (msg.value < i_entranceFee) {
             revert Raffle__SendMoreToEnterRaffle();
         }
 
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
-
-
     }
-
 
     function pickWinner() external {
-       if( block.timestamp - s_lastTimeStamp > i_interval)
-       {
-        revert(); 
-       }
+        if (block.timestamp - s_lastTimeStamp > i_interval) {
+            revert();
+        }
 
-        requestId = s_vrfCoordinator.requestRandomWords(
-        VRFV2PlusClient.RandomWordsRequest({
-        keyHash: s_keyHash,
-        subId: s_subscriptionId,
-        requestConfirmations: requestConfirmations,
-        callbackGasLimit: callbackGasLimit,
-        numWords: numWords,
-        extraArgs: VRFV2PlusClient._argsToBytes(
-          // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
-          VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
-        )
-      })
-    );
-
-
-
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(
+            VRFV2PlusClient.RandomWordsRequest({
+                keyHash: i_keyHash,
+                subId: i_subscriptionId,
+                requestConfirmations: REQUEST_CONFIRMATIONS,
+                callbackGasLimit: i_CallbackGasLimit,
+                numWords: NUM_WORDS,
+                extraArgs: VRFV2PlusClient._argsToBytes(
+                    // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                    VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+                )
+            })
+        );
     }
 
-
-    function getEntranceFee() public view returns(uint256)
-    {
+    function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal virtual override {
-
-    }
-
-
-
-
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal virtual override {}
 }
