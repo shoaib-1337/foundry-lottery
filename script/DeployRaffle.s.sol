cb6d085fd0506c9428e357475ca1af80c006f65e
Shoaib <sali18089@gmail.com>
2026-03-15 22:40:10 +0500

    Added Deployment script and helper config

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.19 ;
 
 import {Script} from "forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
-contract DeployeRaffle is Script{
+contract DeployRaffle is Script {
+    function run() public {
+        // deploy logic
+    }
 
+    function deployContract() public returns (Raffle, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig();
+        Raffle raffle = new Raffle(
+            helperConfig.getConfigByChainId(31337).entranceFee,
+            helperConfig.getConfigByChainId(31337).interval,
+            helperConfig.getConfigByChainId(31337).vrfCoordinator,
+            helperConfig.getConfigByChainId(31337).gasLane,
+            helperConfig.getConfigByChainId(31337).subscriptionId,
+            uint32(helperConfig.getConfigByChainId(31337).callbackGasLimit)
+        );
+        return (raffle, helperConfig);
+    }
 }
